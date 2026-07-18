c54069507eb66f27b903fc3e50d65fc98c453998
Alberto-Vidali <alby91v@gmail.com>
2026-07-18 17:38:37 +0200

    replaced console-logs

--- javascript.js
+++ javascript.js
@@ -15,48 +15,46 @@ function playGame() {
 
     function playRound(humanChoice, computerChoice) {
         if (humanChoice === computerChoice) {
-            console.log(`Draw! You opponent chosed also ${computerChoice}`);
+            document.querySelector(".round-results").textContent = `Draw! Opponent chosed also ${computerChoice}`;
         } else switch (humanChoice) {
             case "rock":
                 if (computerChoice === "paper") {
-                    console.log("You lost! Rock is beaten by Paper");
+                    document.querySelector(".round-results").textContent = `You lost! Rock is beaten by Paper`;
                     computerScore++;
                 } else {
-                    console.log("You won! Rock beats Scissors");
+                    document.querySelector(".round-results").textContent = `You won! Rock beats Scissors`;
                     humanScore++;
                 }
                 break;
             case "paper":
                 if (computerChoice === "rock") {
-                    console.log("You won! Paper beats Rock");
+                    document.querySelector(".round-results").textContent = `You won! Paper beats Rock`;
                     humanScore++;
                 } else {
-                    console.log("You lost! Paper is beaten by Scissors");
+                    document.querySelector(".round-results").textContent = `You lost! Paper is beaten by Scissors`;
                     computerScore++;
                 }
                 break;
             case "scissors":
                 if (computerChoice === "rock") {
-                    console.log("You lost! Scissor is beaten by Rock");
+                    document.querySelector(".round-results").textContent = `You lost! Scissor is beaten by Rock`;
                     computerScore++;
                 } else {
-                    console.log("You won! Rock beats Scissors");
+                    document.querySelector(".round-results").textContent = `You won! Scissors beats Paper`;
                     humanScore++;
                 }
         }
     }
-    
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(prompt("Enter your choice")), getComputerChoice());
-    }
-    
-    if (humanScore > computerScore) {
-        alert("You won the game!");
-    } else if (humanScore < computerScore) {
-        alert("You lost the game!");
-    } else {
-        alert("Game resulted in a draw!");
-    }
+
+    document.querySelector(".rock").addEventListener("click", (event) => {
+        playRound("rock", getComputerChoice());
+    })
+    document.querySelector(".paper").addEventListener("click", (event) => {
+        playRound("paper", getComputerChoice());
+    })
+    document.querySelector(".scissors").addEventListener("click", (event) => {
+        playRound("scissors", getComputerChoice());
+    })
     
 }
 
